0613121de15bcd8c767385adc64e5e3e3ea04596
Sebastian Jensen <sebastianthegreatful@gmail.com>
2018-03-26 22:04:07 +0200

    Run geoiplookup command and return raw output

--- src/main/kotlin/dk/fitfit/ip2country/api/ApiApplication.kt
+++ src/main/kotlin/dk/fitfit/ip2country/api/ApiApplication.kt
@@ -2,10 +2,44 @@ package dk.fitfit.ip2country.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+import java.util.*
+
 
 @SpringBootApplication
 open class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
+}
+
+@RestController
+class Ip2CountryController {
+    @GetMapping("/ip2country/{ip}")
+    fun ip2country(@PathVariable ip: String): String {
+        val locale: Locale = ip2locale(ip)
+        return locale.displayCountry
+    }
+
+    private fun ip2locale(ip: String): Locale {
+        val output = cmd(listOf("geoiplookup", ip))
+        return Locale("", output)
+    }
+
+    private fun cmd(command: List<String>): String {
+        val process = ProcessBuilder(command).start()
+        return readStream(process.inputStream)
+    }
+
+    private fun readStream(stream: InputStream): String {
+        Scanner(stream, StandardCharsets.UTF_8.toString()).use({ scanner ->
+            scanner.useDelimiter("\\A")
+            return if (scanner.hasNext()) scanner.next() else ""
+        })
+    }
+
 }
